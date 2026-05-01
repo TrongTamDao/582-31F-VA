9b91e30135d15f10e8cfc29fed978564c401e6a7
TrongTamDao <daotrongtam80@gmail.com>
2026-04-30 20:22:19 -0400

    dom-asyn

--- class03/dom-asyn.js
+++ class03/dom-asyn.js
@@ -1,17 +1,39 @@
-const btn = document.getElementById("load-btn");
+// let's load in DOM
+const button = document.getElementById("load_button");
 const output = document.getElementById("output");
+const input_field = document.getElementById("inp");
 
-function fakeLoad() {
-  let promise = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Data loaded successfully!");
-    }, 1000);
+function input_check(str) {
+  let promise = new Promise((resolve, reject) => {
+    if (str == "Hello") {
+      // validation
+      setTimeout(() => {
+        resolve("Content loaded"); // validation passed
+      }, 3000); // wait for 3 seconds
+    } else {
+      setTimeout(() => {
+        reject("Content didn't load"); // validation didn't pass
+      }, 3000); // wait for 3 seconds
+    }
   });
-  return promise;
+  return promise; // return promise
 }
-btn.addEventListener("click", () => {
+
+// let's add our function to the button
+button.addEventListener("click", () => {
+  // first we update the text
   output.textContent = "Loading...";
-  fakeLoad().then((result) => {
-    output.textContent = result;
-  });
+
+  // then we update with the actual result
+  input_check(input_field.value)
+    .then((result) => {
+      // successful promise
+      output.textContent = result;
+      input_field.value = "";
+    })
+    .catch((error) => {
+      // rejected promise
+      output.textContent = error;
+      input_field.value = "";
+    });
 });
