b245b1c9b2d1f1c73c9f11c05a8c67ccb9ef568a
TrongTamDao <daotrongtam80@gmail.com>
2026-05-01 23:27:29 -0400

    First version

---
+++ class04/lab/app.js
@@ -0,0 +1,100 @@
+const btnStd = document.getElementById("load-student-btn");
+const btnCourses = document.getElementById("load-courses-btn");
+const btnClear = document.getElementById("clear-btn");
+const status = document.getElementById("status");
+const studentData = document.getElementById("student-container");
+const courseData = document.getElementById("courses-container");
+
+const auth = true;
+
+const student = {
+  name: "tam",
+  program: "Web designer",
+  semester: "Winter",
+  bio: "28 years old",
+};
+
+const courses = [
+  { code: "WIP2", title: "Web Interface Programming 2" },
+  { code: "AWP", title: "Advanced Programming" },
+  { code: "DB2", title: "Database Management Systems 2" },
+];
+
+//Create a Promise to Load Student Data
+function loadStudentData() {
+  return new Promise((resolve, reject) => {
+    if (auth) {
+      resolve(student);
+    } else {
+      reject("student not found");
+    }
+  });
+}
+
+//Render Student Data in the DOM
+const renderStudent = function (student) {
+  studentData.innerHTML = `
+    <p>Student name: ${student.name}</p>
+    <p>Program: ${student.program}</p>
+    <p>Semester: ${student.semester}</p>
+    <p>Bio: ${student.bio}</p>
+  `;
+};
+
+//Connect the Load Student Button
+btnStd.addEventListener("click", () => {
+  status.textContent = "Loading...";
+  setTimeout(() => {
+    loadStudentData()
+      .then((student) => {
+        renderStudent(student);
+        status.textContent = "Loaded Successfully";
+      })
+      .catch((error) => {
+        studentData.textContent = error;
+        console.log(error);
+      });
+  }, 1000);
+});
+
+//Create a Promise to Load Course Data
+function getCoursesData() {
+  return new Promise((resolve, reject) => {
+    if (auth) {
+      resolve(courses);
+    } else {
+      resolve("could not retrive course data");
+    }
+  });
+}
+
+//Render Course Data
+const renderCourses = function (course) {
+  courseData.innerHTML += `<p>Code: ${course.code}, title: ${course.title}</p>`;
+};
+
+//Connect the Load Courses Button
+btnCourses.addEventListener("click", () => {
+  status.textContent = "Loading...";
+  setTimeout(() => {
+    getCoursesData()
+      .then((courses) => {
+        courseData.innerHTML = "";
+        courses.forEach((course) => {
+          renderCourses(course);
+        });
+        status.textContent = "Loaded Successfully";
+      })
+      .catch((error) => {
+        courseData.textContent = error;
+        status.textContent = "Error loading data";
+      });
+  }, 1000);
+});
+
+//Clear Button
+btnClear.addEventListener("click", () => {
+  studentData.innerHTML = "";
+  courseData.innerHTML = "";
+  status.textContent = "Ready";
+});
