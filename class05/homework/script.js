8e1c9d348af893c69a4407d71b8b71a29e42222d
TrongTamDao <daotrongtam80@gmail.com>
2026-05-07 09:45:59 -0400

    save new changes

---
+++ class05/homework/script.js
@@ -0,0 +1,11 @@
+const loadbtn = document.getElementById("loadbtn");
+const status = document.getElementById("status");
+const output = document.getElementById("output");
+
+const fetchData = fetch("https://jsonplaceholder.typicode.com/posts");
+
+fetchData
+  .then((response) => response.json())
+  .catch((error) => console.log(error));
+
+console.log(response[0].id);
