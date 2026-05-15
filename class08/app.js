fdd17cc88979857e9dbef8bc0c6dae2069646ae7
TrongTamDao <daotrongtam80@gmail.com>
2026-05-14 23:21:37 -0400

    add html js

---
+++ class08/app.js
@@ -0,0 +1,5 @@
+// DOM loading
+const btnLoad = document.getElementById("load-post");
+const btnClear = document.getElementById("clear");
+const status = document.getElementById("status");
+const output = document.getElementById("output");
