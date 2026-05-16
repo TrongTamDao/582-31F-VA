98a3d6944c2def578f2ebf42ba16691386f7023a
TrongTamDao <daotrongtam80@gmail.com>
2026-05-16 19:53:05 -0400

    adding renderUserCard

--- class08/app.js
+++ class08/app.js
@@ -15,7 +15,11 @@ const clearDashboard = function () {
   status.textContent = "Ready";
 };
 
-const loadUser = function () {
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((post) => console.log(post));
+
+const loadUsers = function () {
   return fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => {
       if (!response.ok) {
@@ -24,4 +28,31 @@ const loadUser = function () {
       return response.json();
     })
     .catch((error) => console.log(`Fail to load post: ${error.message}`));
+};
+
+loadUsers().then((post) => console.log(post));
+
+const renderUderCard = function (user) {
+  output.innerHTML = `
+            <div class="col mx-3">
+                <div class="card h-100 shadow-sm bg-success-subtle" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${user.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${user.username}</h6>
+                        <h6 class="card-text"><strong>Address</strong></h6>
+                                <p><strong>City:</strong> ${user.address.city}</p>
+                                <p><strong>Street:</strong> ${user.address.street}</p>
+                                <p><strong>Suit:</strong> ${user.address.suite}<p>
+                                <p><strong>Email:</strong>${user.email}</p>
+                                <p><strong>Phone:</strong> ${user.phone}</p>
+                                <p><strong>Company name:</strong> ${user.company.name}</p>
+                                <p><strong>Website:</strong> ${user.website} </p>
+                    </div>
+                </div>
+            </div>
+        `;
+};
+
+const loadPostsForUser = function (user, postsContainer) {
+  fetch("https://jsonplaceholder.typicode.com/posts/${user}");
 };
