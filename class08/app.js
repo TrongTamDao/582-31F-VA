577488ef213c6d0a5bff05d656c7c29d13c91f29
TrongTamDao <daotrongtam80@gmail.com>
2026-05-17 00:39:34 -0400

    all functions added and worked

--- class08/app.js
+++ class08/app.js
@@ -1,5 +1,5 @@
 // DOM loading
-const btnLoad = document.getElementById("load-post");
+const btnLoadUser = document.getElementById("load-user");
 const btnClear = document.getElementById("clear");
 const status = document.getElementById("status");
 const output = document.getElementById("output");
@@ -15,9 +15,9 @@ const clearDashboard = function () {
   status.textContent = "Ready";
 };
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
-  .then((post) => console.log(post));
+// fetch("https://jsonplaceholder.typicode.com/posts/")
+//   .then((response) => response.json())
+//   .then((posts) => console.log(posts));
 
 const loadUsers = function () {
   return fetch("https://jsonplaceholder.typicode.com/users")
@@ -27,32 +27,71 @@ const loadUsers = function () {
       }
       return response.json();
     })
-    .catch((error) => console.log(`Fail to load post: ${error.message}`));
+    .catch((error) => console.log(`Fail to load user: ${error.message}`));
 };
 
-loadUsers().then((post) => console.log(post));
-
-const renderUderCard = function (user) {
-  output.innerHTML = `
-            <div class="col mx-3">
-                <div class="card h-100 shadow-sm bg-success-subtle" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">${user.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">${user.username}</h6>
-                        <h6 class="card-text"><strong>Address</strong></h6>
-                                <p><strong>City:</strong> ${user.address.city}</p>
-                                <p><strong>Street:</strong> ${user.address.street}</p>
-                                <p><strong>Suit:</strong> ${user.address.suite}<p>
-                                <p><strong>Email:</strong>${user.email}</p>
-                                <p><strong>Phone:</strong> ${user.phone}</p>
-                                <p><strong>Company name:</strong> ${user.company.name}</p>
-                                <p><strong>Website:</strong> ${user.website} </p>
-                    </div>
-                </div>
-            </div>
-        `;
+const renderUserCard = function (user) {
+  output.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="col">
+      <div class="card h-100 shadow-sm bg-success-subtle">
+        <div class="card-body">
+          <h5 class="card-title">${user.name}</h5>
+          <p>
+            <strong>Email:</strong> ${user.email}
+          </p>
+          <p>
+            <strong>Phone:</strong> ${user.phone}
+          </p>
+          <p>
+            <strong>City:</strong> ${user.address.city}
+          </p>
+          <p>
+            <strong>Company name:</strong> ${user.company.name}
+          </p>
+          <button id="loadPost-${user.id}">Load post</button>
+          <div id="post-${user.id}"></div>
+        </div>
+      </div>
+    </div>,
+    `,
+  );
 };
 
-const loadPostsForUser = function (user, postsContainer) {
-  fetch("https://jsonplaceholder.typicode.com/posts/${user}");
+const loadPostsForUsers = function (user) {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${user.id}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((error) => console.log(`Fail to load user: ${error.message}`));
 };
+
+const renderPost = function (post, postContainer) {
+  postContainer.innerHTML = `
+        ${post.body}
+
+    `;
+};
+
+btnLoadUser.addEventListener("click", () => {
+  output.innerHTML = "";
+  loadUsers().then((users) => {
+    users.forEach((user) => {
+      renderUserCard(user);
+
+      const btnLoadPost = document.getElementById(`loadPost-${user.id}`);
+      //   console.log(btnLoadPost);
+      const postContainer = document.getElementById(`post-${user.id}`);
+      //   console.log(postContainer);
+      btnLoadPost.addEventListener("click", () => {
+        loadPostsForUsers(user).then((post) => {
+          renderPost(post, postContainer);
+        });
+      });
+    });
+  });
+});
