805962b5d7861902b5d5e300b3697ad9f7cb7170
TrongTamDao <daotrongtam80@gmail.com>
2026-06-18 20:27:43 -0400

    create Team class

---
+++ class17/lab/team-card.js
@@ -0,0 +1,35 @@
+export class Team {
+  static isValidPoints(points) {
+    return typeof points === "number" && points >= 0;
+  }
+
+  static fromObject(data) {
+    return new Team(
+      data.id,
+      data.name,
+      data.group,
+      data.points,
+      data.played,
+      data.goalDifference,
+    );
+  }
+
+  constructor(id, name, group, points, played, goalDifference) {
+    this._id = id;
+    this._name = name;
+    this._group = group;
+    this._points = points;
+    this._played = played;
+    this.goalDifference = goalDifference;
+  }
+
+  get summary() {
+    return `${this._name} - ${this._group} - ${this._points}`;
+  }
+
+  set points(value) {
+    if (Team.isValidPoints(value)) {
+      this._points = value;
+    } else throw new Error("Point must be a non-negative number");
+  }
+}
